28a72bf90aaafa2ed9cbf0cfc52794a95338608e
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2023-05-27 22:55:03 +0900

    feat: subnet and nat

--- main.tf
+++ main.tf
@@ -2,33 +2,33 @@ provider "aws" {
   region  = "ap-northeast-1"
 }
 
-module "web_server" {
-  source = "./http_server/"
-  instance_type = "t2.micro"
-}
+# module "web_server" {
+#   source = "./http_server/"
+#   instance_type = "t2.micro"
+# }
 
-data "aws_iam_policy_document" "allow_describe_regions" {
-  statement {
-    effect = "Allow"
-    actions = ["ec2:DescribeRegions"]
-    resources = ["*"]
-  }
-}
+# data "aws_iam_policy_document" "allow_describe_regions" {
+#   statement {
+#     effect = "Allow"
+#     actions = ["ec2:DescribeRegions"]
+#     resources = ["*"]
+#   }
+# }
 
-module "describe_regions_for_ec2" {
-  source = "./iam_role"
-  name = "describe-regions-for-ec2"
-  identifier = "ec2.amazonaws.com"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
-}
+# module "describe_regions_for_ec2" {
+#   source = "./iam_role"
+#   name = "describe-regions-for-ec2"
+#   identifier = "ec2.amazonaws.com"
+#   policy = data.aws_iam_policy_document.allow_describe_regions.json
+# }
 
-output "public_dns" {
-  value = module.web_server.public_dns
-}
+# output "public_dns" {
+#   value = module.web_server.public_dns
+# }
 
-module "aws_s3_bucket_elb" {
-  source = "./storage"
-}
+# module "aws_s3_bucket_elb" {
+#   source = "./storage"
+# }
 
 module "aws_vpc" {
   source = "./network"
--- network/main.tf
+++ network/main.tf
@@ -14,3 +14,60 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
 }
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+# public subnetの設定
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# private subnetの設定
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.64.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+# NATゲートウェイの設定
+# provate subnetからインターネットに接続するために必要
+# NATゲートウェイにはEIPが必要？ => インスタンス起動のたびにIP変わるから
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id = aws_subnet.public.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private" {
+  route_table_id = aws_route_table.private.id
+  nat_gateway_id = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
