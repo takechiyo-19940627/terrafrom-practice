38b523ae10435ce6d8cbf7f6906a09614f320171
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2023-05-27 13:16:23 +0900

    feat: add iam role module

--- main.tf
+++ main.tf
@@ -7,6 +7,21 @@ module "web_server" {
   instance_type = "t2.micro"
 }
 
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+module "describe_regions_for_ec2" {
+  source = "./iam_role"
+  name = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
 output "public_dns" {
   value = module.web_server.public_dns
 }
